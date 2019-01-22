fa50b1ae252f4fb26bce9b8130eeb2c5198a3333
sinahk <sinahk1992@yahoo.com>
2019-01-22 13:21:31 +0330

    initial

--- SillyCalc/ViewController.swift
+++ SillyCalc/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    @IBAction func numberPressed(_ sender: Any) {
+        let digit = (sender as AnyObject).currentTitle!
+        switch digit {
+        case "1"?:
+            display.text = "1"
+        case "2"?:
+            display.text = "2"
+        case "3"?:
+            display.text = "3"
+        default:
+            display.text = digit
+        }
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
